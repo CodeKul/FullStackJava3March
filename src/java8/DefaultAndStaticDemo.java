1d809f7d1ca1dd381ade6d9037d56eb0c619e19c
vaibhav <codekul.vaibhav@gmail.com>
2021-05-14 17:53:35 +0530

    theory of DefaultAndStaticDemo and Stored procedure

--- src/java8/DefaultAndStaticDemo.java
+++ src/java8/DefaultAndStaticDemo.java
@@ -2,6 +2,15 @@ package java8;
 
 import java.util.stream.Stream;
 
+/**
+ * interface in java can only have abstract methods.
+ * All the methods of interfaces are public & abstract by default.
+ * Java 8 allows the interfaces to have default and static methods.
+ * The reason we have default methods in interfaces is to allow the developers to
+ * add new methods to the interfaces without affecting the classes that implements these
+ * interfaces.
+ *
+ */
 public class DefaultAndStaticDemo implements  Myinterface3,MyInterface4{
 
     public static void main(String[] args) {
